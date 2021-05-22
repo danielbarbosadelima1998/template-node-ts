fbb42d72681e0a37fd5491d2ce4d9746ab252a9f
Daniel Barbosa de Lima <daniel.lima@ikatec.com.br>
2021-05-22 19:02:42 -0300

    eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello world" });
-});
+app.get('/', (req, res) => res.json({ message: 'hello world' }));
 
 app.listen(3333);
