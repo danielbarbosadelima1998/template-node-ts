f971c00c420d550116b958dc4a8f255fc27a8231
Daniel Barbosa de Lima <daniel.lima@ikatec.com.br>
2021-05-23 12:27:10 -0300

    babel

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'hello world' }));
+app.get('/', (req, res) => res.send('Bem vindo a api'));
 
-app.listen(3333);
+app.listen(3333, () => console.log('Servidor rodando na porta 3333'));
